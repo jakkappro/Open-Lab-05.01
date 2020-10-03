d5635b0580c1072826932f827b4d6839f59f1d63
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-03 10:00:54 +0200

    I made function to return century

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            int ret = year % 100 == 0 ? year / 100 : year / 100 +1;
+
+            string sret = ret.ToString();
+
+            if(ret == 11 || ret == 12)
+                return sret + "th century";
+
+            if (sret.ToCharArray()[sret.Length - 1] == '1')
+            {
+                return sret + "st century";
+            }
+            if (sret.ToCharArray()[sret.Length - 1] == '2')
+            {
+                return sret + "nd century";
+            }
+            if (sret.ToCharArray()[sret.Length - 1] == '3')
+            {
+                return sret + "rd century";
+            }
+
+            return sret + "th century";
         }
     }
 }
